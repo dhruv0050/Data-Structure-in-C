afe82db66fe24854fb79b83a574232e7998c0b8d
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2024-10-15 18:19:11 +0530

    Update Stacks.c

--- Stacks.c
+++ Stacks.c
@@ -1,81 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-    int value; 
-    int* next;
-};
+// Definition of a node in the doubly linked list
+typedef struct Node {
+   int d;
+   struct Node* next;
+   struct Node* prev;
+} Node;
 
-void enqueue(struct node* f, struct node* r){
-    struct node* n = (struct node*)malloc(sizeof(struct node));
-    if(n==NULL){
-        printf("No More Memory can be allocated !\n");
-    }
-    else{
-        if(f==NULL){
-            f=r=n;
-        }
-        else{
-            r->next = n;
-            r=n;
-        }
-    }
+// Function to push a value onto the stack
+void pushtolist(Node** top, int value) {
+   Node* newnode = (Node*)malloc(sizeof(Node));
+   newnode->d = value;
+   newnode->next = NULL;
+   newnode->prev = *top;
+
+   if (*top != NULL) {
+       (*top)->next = newnode;
+   }
+   *top = newnode;
 }
 
-int dequeue(struct node*f, struct node* r){
-    int value = -1;
-    if(f==NULL){
-        printf("Stack Underflow !");
-    }
-    else{
-        struct node* ptr = f;
-        value = ptr->value;
-        f=ptr->next;
-        free(ptr);
-    }
-    return value;
+// Function to display the stack from top to bottom
+void displaylist(Node* top) {
+   Node* temp = top;
+   if (temp == NULL) {
+       printf("Empty Stack\n");
+       return;
+   }
+   while (temp != NULL) {
+       printf("%d\n", temp->d);
+       temp = temp->prev;
+   }
 }
 
-void traverse(struct node* front, struct node* rear){
-    while(front->next!=rear){
-        printf("Queue is: ");
-        printf("");
-    }
+// Function to pop a value from the stack
+void popfromlist(Node** top) {
+   if (*top == NULL) {
+       printf("Empty Stack\n");
+       return;
+   }
+   Node* temp = *top;
+   *top = (*top)->prev;
+   if (*top != NULL) {
+       (*top)->next = NULL;
+   }
+   free(temp);
 }
 
-int main(){
-    struct node* front = NULL;
-    struct node* rear = NULL;
+// Structure for Stack
+typedef struct {
+   Node* top;
+} Stack;
 
-    int choice, value;
-    while (1) {
-        printf("\nChoose an option: \n");
-        printf("1. Enqueue\n");
-        printf("2. Dequeue\n");
-        printf("3. View Queue\n");
-        printf("4. Exit\n");
-        scanf("%d", &choice);
+// Initialize a stack
+void initStack(Stack* stack) {
+   stack->top = NULL;
+}
 
-        switch (choice) {
-            case 1:
-                printf("Enter value to enqueue: ");
-                scanf("%d", &value);
-                enqueue(&q, value);
-                break;
-            case 2:
-                dequeue(&q);
-                break;
-            case 3:
-                traverse(&q);
-                break;
-            case 4:
-                printf("Exiting...\n");
-                free(q.arr);
-                return 0;
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
+// Push a value onto the stack
+void push(Stack* stack, int value) {
+   pushtolist(&stack->top, value);
+}
 
-    return 0;
+// Traverse the stack
+void traverse(Stack* stack) {
+   displaylist(stack->top);
+}
+
+// Pop a value from the stack
+void pop(Stack* stack) {
+   popfromlist(&stack->top);
+}
+
+// Peek at the top value of the stack
+void peek(Stack* stack) {
+   if (stack->top == NULL) {
+       printf("Empty Stack\n");
+   } else {
+       printf("%d\n", stack->top->d);
+   }
+}
+
+// Main function
+int main() {
+   Stack st;
+   initStack(&st);
+
+   while (1) {
+       int choice;
+       printf("1. Push\n");
+       printf("2. Peek\n");
+       printf("3. Traverse\n");
+       printf("4. Pop\n");
+       printf("5. Exit\n");
+       printf("Enter Choice: ");
+       scanf("%d", &choice);
+
+       switch (choice) {
+           case 1: {
+               int num;
+               printf("Enter Data: ");
+               scanf("%d", &num);
+               push(&st, num);
+               break;
+           }
+           case 2:
+               peek(&st);
+               break;
+           case 3:
+               traverse(&st);
+               break;
+           case 4:
+               pop(&st);
+               break;
+           case 5:
+               printf("Exited!\n");
+               exit(0);
+       }
+   }
+   return 0;
 }
